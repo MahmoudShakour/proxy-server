7bf965399483edfcac02df0dee4c89fb9d3368b4
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-10-03 20:37:48 +0300

    Make proxy server multi-threaded

--- proxy.c
+++ proxy.c
@@ -6,22 +6,19 @@
 
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-char headers[MAXLINE];
+void *thread(void *data);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 void doit(int fd);
-void read_requesthdrs(rio_t *rp);
-void parse_uri(char *uri, char *filename, char *cgiargs);
-void serve_static(int fd, char *filename, int filesize);
-void get_filetype(char *filename, char *filetype);
-void serve_dynamic(int fd, char *filename, char *cgiargs);
-void clienterror(int fd, char *cause, char *errnum,
-                 char *shortmsg, char *longmsg);
+void read_requesthdrs(rio_t *rp, char *method, char *filename, char *version, char *uri, char *headers);
+void parse_uri(char *uri, char *filename, char *port);
 
 int main(int argc, char **argv)
 {
-    int listenfd, connfd;
-    char hostname[MAXLINE], port[MAXLINE];
+    int listenfd, *connfd;
+    // char hostname[MAXLINE], port[MAXLINE];
     socklen_t clinetlen;
     struct sockaddr_storage clientaddr;
+    pthread_t tid;
 
     if (argc != 2)
     {
@@ -34,21 +31,31 @@ int main(int argc, char **argv)
     while (1)
     {
         clinetlen = sizeof(clientaddr);
-        connfd = Accept(listenfd, (SA *)&clientaddr, &clinetlen);
-        Getnameinfo((SA *)&clientaddr, clinetlen, hostname, MAXLINE, port, MAXLINE, 0);
-        printf("Accepted connetion from (%s,%s)\n", hostname, port);
-        doit(connfd);
-        Close(connfd);
+        connfd = Malloc(sizeof(int));
+        *connfd = Accept(listenfd, (SA *)&clientaddr, &clinetlen);
+        Pthread_create(&tid, NULL, thread, connfd);
+        // doit(connfd);
+        // Close(connfd);
     }
     return 0;
 }
 
-char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-char filename[MAXLINE], port[MAXLINE];
+void *thread(void *data)
+{
+    int fd = *((int *)data);
+    printf("%d\n\n\n",fd);
+    pthread_detach(pthread_self());
+    Free(data);
+    doit(fd);
+    Close(fd);
+    return NULL;
+}
+
 void doit(int fd)
 {
-    rio_t rio;
-    rio_t tiny_rio;
+    char headers[MAXLINE], buf[MAXLINE], method[MAXLINE];
+    char filename[MAXLINE], port[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    rio_t rio, tiny_rio;
 
     /* Read request line and headers */
     Rio_readinitb(&rio, fd);
@@ -62,46 +69,42 @@ void doit(int fd)
                     "Tiny does not implement this method");
         return;
     }
-    /* Parse URI from GET request */
+
     parse_uri(uri, filename, port);
-    read_requesthdrs(&rio);
+    read_requesthdrs(&rio, method, filename, version, uri, headers);
 
-
-    // printf("uri: (%s) (%d) \n", uri, strlen(uri));
-    // printf("filename: (%s) (%d) \n", filename, strlen(filename));
-    // printf("port: (%s) \n", port);
-    // printf("method: (%s)\n", method);
     strcpy(uri, "localhost"); // +7
     int tinyfd = Open_clientfd(uri, port);
     Rio_readinitb(&tiny_rio, tinyfd);
-   
+
     Rio_writen(tinyfd, headers, strlen(headers));
     int n;
-    while ((n=Rio_readlineb(&tiny_rio, buf, MAXLINE)) > 0)
+    while ((n = Rio_readlineb(&tiny_rio, buf, MAXLINE)) > 0)
     {
         printf("%s", buf);
-        Rio_writen(fd, buf, strlen(buf));
+        Rio_writen(fd, buf, n);
     }
 }
-// GET http://localhost:20000/godzilla.jpg HTTP/1.0
-// GET /godzilla.jpg HTTP/1.0
-void read_requesthdrs(rio_t *rp)
+// GET http://localhost:20000/home.html HTTP/1.0
+
+void read_requesthdrs(rio_t *rp, char *method, char *filename, char *version, char *uri, char *headers)
 {
-    sprintf(headers,"%s %s %s\r\n",method,filename,version);
+    sprintf(headers, "%s %s %s\r\n", method, filename, version);
     sprintf(headers, "%s Connection: close\r\n", headers);
     sprintf(headers, "%s Proxy-Connection: close\r\n", headers);
-    sprintf(headers, "%s Host: %s\r\n\r\n", headers,uri);
+    sprintf(headers, "%s Host: %s\r\n\r\n", headers, uri);
 
     char buf[MAXLINE];
     Rio_readlineb(rp, buf, MAXLINE);
     sprintf(headers, "%s%s", headers, buf);
-    
+
     while (strcmp(buf, "\r\n"))
     {
         Rio_readlineb(rp, buf, MAXLINE);
         sprintf(headers, "%s%s", headers, buf);
     }
 }
+
 void parse_uri(char *uri, char *filename, char *port)
 {
     char *ptr, *colon;
@@ -135,8 +138,7 @@ void parse_uri(char *uri, char *filename, char *port)
     }
 }
 
-void clienterror(int fd, char *cause, char *errnum,
-                 char *shortmsg, char *longmsg)
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg)
 {
     exit(1);
 }
